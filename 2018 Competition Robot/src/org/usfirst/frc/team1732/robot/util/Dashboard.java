961b098c81dae4f5953828867249770597443274
Jay Schauer <lacrossejason@gmail.com>
2018-02-27 16:23:56 -0600

    set thread to be daemon

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/util/Dashboard.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/util/Dashboard.java
@@ -9,10 +9,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Dashboard {
 
 	public Dashboard() {
-		new Thread(this::loop).start();
+		Thread thread = new Thread(this::loop);
+		thread.setDaemon(true);
+		thread.start();
 	}
 
-	private void sleepExactly() {
+	private void sleep() {
 		try {
 			Thread.sleep(40);
 		} catch (InterruptedException e) {
@@ -21,39 +23,40 @@ public class Dashboard {
 	}
 
 	private void loop() {
-		while (true) {
+		while (!Thread.interrupted()) {
 			entries.forEach(this::call);
-			sleepExactly();
+			sleep();
 		}
 	}
-	
+
 	private void call(Entry e) {
 		e.putToDashboard();
 	}
-	
+
 	private static LinkedList<Entry> entries;
-	
+
 	public void add(String name, Supplier<?> sup) {
 		entries.add(new Entry(name, sup));
 	}
-	
+
 	private class Entry {
 		private final String name;
 		private final Supplier<?> sup;
+
 		public Entry(String name, Supplier<?> sup) {
 			this.name = name;
 			this.sup = sup;
 		}
-		
+
 		public void putToDashboard() {
 			Object o = sup.get();
-			if(o instanceof Number) {
+			if (o instanceof Number) {
 				SmartDashboard.putNumber(name, ((Number) o).doubleValue());
-			}else if(o instanceof Boolean) {
+			} else if (o instanceof Boolean) {
 				SmartDashboard.putBoolean(name, (Boolean) o);
-			}else if(o instanceof String) {
+			} else if (o instanceof String) {
 				SmartDashboard.putString(name, (String) o);
-			}else if(o instanceof Sendable) {
+			} else if (o instanceof Sendable) {
 				SmartDashboard.putData((Sendable) o);
 			}
 		}
