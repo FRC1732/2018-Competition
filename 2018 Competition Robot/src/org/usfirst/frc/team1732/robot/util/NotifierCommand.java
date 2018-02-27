d0ee1f7e125c1083f49dd1665b475032d6dd6e55
Jay Schauer <lacrossejason@gmail.com>
2018-02-27 16:18:58 -0600

    Revert "added notifier command class made from robot-chassis 1"
    This reverts commit 9c2f9bc76e0566f4e4f5f8053e5c163b35cc6324.

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/util/NotifierCommand.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/util/NotifierCommand.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc.team1732.robot.util;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.hal.NotifierJNI;
 
@@ -38,8 +36,7 @@ public abstract class NotifierCommand extends Command {
 
 	@Override
 	protected final void end() {
-		// stop(); don't need to call, stop should have been called when m_isRunning was
-		// set to false
+		stop();
 		whenEnded();
 	}
 
@@ -58,7 +55,7 @@ public abstract class NotifierCommand extends Command {
 	private final Thread m_thread;
 
 	// The lock for the process information.
-	private final ReentrantLock m_processLock = new ReentrantLock();
+	// private final ReentrantLock m_processLock = new ReentrantLock();
 
 	// The C pointer to the notifier object. We don't use it directly, it is
 	// just passed to the JNI bindings.
@@ -75,11 +72,8 @@ public abstract class NotifierCommand extends Command {
 	// directly from/to memory instead of the CPU cache
 	private volatile boolean m_isRunning = false;
 
-	// timer for time since started;
-	private final Timer timer;
-
 	@Override
-	protected final void finalize() {
+	protected void finalize() {
 		int handle = m_notifier.getAndSet(0);
 		NotifierJNI.stopNotifier(handle);
 		// Join the thread to ensure the handler has exited.
@@ -97,7 +91,7 @@ public abstract class NotifierCommand extends Command {
 	/**
 	 * Update the alarm hardware to reflect the next alarm.
 	 */
-	private final void updateAlarm() {
+	private void updateAlarm() {
 		int notifier = m_notifier.get();
 		if (notifier == 0) {
 			return;
@@ -124,22 +118,22 @@ public abstract class NotifierCommand extends Command {
 					break;
 				}
 
-				Runnable handler = null;
-				m_processLock.lock();
-				try {
-					handler = m_handler;
-					m_expirationTime += m_period;
-					updateAlarm();
-				} finally {
-					m_processLock.unlock();
-				}
+				// Runnable handler = null;
+				// m_processLock.lock();
+				// try {
+				// handler = m_handler;
+				m_expirationTime += m_period;
+				updateAlarm();
+				// } finally {
+				// m_processLock.unlock();
+				// }
 
-				if (handler != null && m_isRunning) {
-					handler.run();
-					// if (m_isRunning) {
-					// m_handler.run();
+				// if (handler != null && m_isRunning) {
+				// handler.run();
+				if (m_isRunning) {
+					m_handler.run();
 					if (isDone()) {
-						stop();
+						m_isRunning = false;
 					}
 				}
 			}
@@ -158,7 +152,6 @@ public abstract class NotifierCommand extends Command {
 			System.exit(1);
 		});
 		m_thread.start();
-		timer = new Timer();
 	}
 
 	/**
@@ -170,17 +163,15 @@ public abstract class NotifierCommand extends Command {
 	 *            Period in seconds to call the handler starting one period after
 	 *            the call to this method.
 	 */
-	private final void startPeriodic() {
-		m_processLock.lock();
-		try {
-			m_expirationTime = RobotController.getFPGATime() * 1e-6 + m_period;
-			updateAlarm();
-			timer.reset();
-			timer.start();
-			m_isRunning = true;
-		} finally {
-			m_processLock.unlock();
-		}
+	private void startPeriodic() {
+		// m_processLock.lock();
+		// try {
+		m_expirationTime = RobotController.getFPGATime() * 1e-6 + m_period;
+		updateAlarm();
+		m_isRunning = true;
+		// } finally {
+		// m_processLock.unlock();
+		// }
 	}
 
 	/**
@@ -189,14 +180,9 @@ public abstract class NotifierCommand extends Command {
 	 * queue. If a timer-based call to the registered handler is in progress, this
 	 * function will block until the handler call is complete.
 	 */
-	private final void stop() {
+	private void stop() {
 		NotifierJNI.cancelNotifierAlarm(m_notifier.get());
-		timer.stop();
 		m_isRunning = false;
-	}
-
-	public final double timeSinceStarted() {
-		return timer.get();
 	}
 
 }
