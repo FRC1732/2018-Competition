e9d525cb784c4389d660953881a209ebf0b82c41
Jay Schauer <lacrossejason@gmail.com>
2018-03-08 07:19:24 -0600

    changed Dial to use Notifier

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/input/Dial.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/input/Dial.java
@@ -6,42 +6,36 @@ import org.usfirst.frc.team1732.robot.Robot;
 import org.usfirst.frc.team1732.robot.util.ValueChangeListener;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Notifier;
 
 public class Dial extends Joystick {
 	private final ArrayList<ValueChangeListener> listeners;
 
+	private int prevVal = get();
+
 	public Dial(final int port) {
 		super(port);
 		listeners = new ArrayList<>();
-		new Thread() {
-			int prevVal = get();
-
-			public void run() {
-				while (!this.isInterrupted()) {
-					int currentVal = get();
-					if (currentVal != prevVal) {
-						listeners.forEach(l -> l.valueChanged(currentVal));
-						prevVal = currentVal;
-					}
-					synchronized (this) {
-						try {
-							Thread.sleep(Robot.PERIOD_MS);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				}
+		Notifier thread = new Notifier(() -> {
+			int currentVal = get();
+			if (currentVal != prevVal) {
+				listeners.forEach(l -> l.valueChanged(currentVal));
+				prevVal = currentVal;
 			}
-		}.start();
+		});
+		thread.startPeriodic(Robot.PERIOD_S);
 	}
+
 	/**
 	 * @return The current value of the dial, starting at 0
 	 */
 	public int get() {
 		for (int i = 1; i <= this.getButtonCount(); i++)
-			if (this.getRawButton(i)) return i - 1;
+			if (this.getRawButton(i))
+				return i - 1;
 		return -1;
 	}
+
 	public void addValueChangeListener(ValueChangeListener l) {
 		listeners.add(l);
 	}
