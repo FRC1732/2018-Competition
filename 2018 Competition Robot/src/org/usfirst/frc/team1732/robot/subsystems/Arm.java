5286aeee82d7034badec1c5931dcc58894a62647
Jay Schauer <lacrossejason@gmail.com>
2018-03-09 17:53:39 -0600

    increased accel

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/subsystems/Arm.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/subsystems/Arm.java
@@ -173,10 +173,10 @@ public class Arm extends Subsystem {
 		if (desiredPosition > maxLow && currentPosition < maxLow) {
 			motor.configMotionAcceleration((int) (magicAccel * 0.6), Robot.CONFIG_TIMEOUT);
 		} else if (desiredPosition <= maxLow && currentPosition > maxLow - 100) {
-			motor.configMotionAcceleration((int) (magicAccel * 0.1), Robot.CONFIG_TIMEOUT); // 0.2
+			motor.configMotionAcceleration((int) (magicAccel * 0.15), Robot.CONFIG_TIMEOUT); // 0.2
 			motor.configMotionCruiseVelocity((int) (magicVel * 0.5), Robot.CONFIG_TIMEOUT); // 0.5
 		} else {
-			motor.configMotionAcceleration(magicAccel, Robot.CONFIG_TIMEOUT);
+			motor.configMotionAcceleration((int) (magicAccel * 0.7), Robot.CONFIG_TIMEOUT);
 		}
 		magicGains.selectGains(motor);
 	}
