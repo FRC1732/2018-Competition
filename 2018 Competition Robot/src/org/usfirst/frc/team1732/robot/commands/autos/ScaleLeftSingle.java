fbacc8da12b84bb6b6d4e45c6dc5f2ad5ae9b77d
Jay Schauer <lacrossejason@gmail.com>
2018-03-09 19:43:41 -0600

    added scale left without cross

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/autos/ScaleLeftSingle.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/autos/ScaleLeftSingle.java
@@ -4,11 +4,14 @@ import org.usfirst.frc.team1732.robot.Robot;
 import org.usfirst.frc.team1732.robot.autotools.DriverStationData;
 import org.usfirst.frc.team1732.robot.commands.primitive.ArmElevatorSetPosition;
 import org.usfirst.frc.team1732.robot.commands.primitive.ArmMagicPosition;
+import org.usfirst.frc.team1732.robot.commands.primitive.DriveTime;
 import org.usfirst.frc.team1732.robot.commands.primitive.FollowVelocityPath;
 import org.usfirst.frc.team1732.robot.commands.primitive.Wait;
 import org.usfirst.frc.team1732.robot.controlutils.motionprofiling.pathing.Path.PointProfile;
 import org.usfirst.frc.team1732.robot.subsystems.Arm;
 import org.usfirst.frc.team1732.robot.subsystems.Elevator;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -26,29 +29,49 @@ public class ScaleLeftSingle extends CommandGroup {
 			profile = Robot.paths.scaleLeftSingleLeftProfile;
 			time = profile.getTotalTimeSec();
 			percent = 0.7;
+			addSequential(new CommandGroup() {
+				{
+					addParallel(new CommandGroup() {
+						{
+							addSequential(new Wait(time * percent));
+							addSequential(
+									new ArmElevatorSetPosition(Arm.Positions.SCALE_HIGH,
+											Elevator.Positions.SCALE_HIGH));
+						}
+					});
+					addSequential(new FollowVelocityPath(profile));
+				}
+			});
+
+			// addSequential(new PreAuto(Arm.Positions.TUCK));
+			// addSequential(new ArmElevatorSetPosition(Arm.Positions.SCALE,
+			// Elevator.Positions.SCALE_HIGH));
+			addSequential(new ManipAutoEject(0.5));
 		} else {
-			profile = Robot.paths.scaleLeftSingleRightProfile;
-			time = profile.getTotalTimeSec();
-			percent = 0.8;
+			addSequential(new DriveTime(0.25, 0.25, NeutralMode.Brake, 5));
+			// profile = Robot.paths.scaleLeftSingleRightProfile;
+			// time = profile.getTotalTimeSec();
+			// percent = 0.8;
 		}
 
 		// makes sure it propertly waits before shooting cube
-		addSequential(new CommandGroup() {
-			{
-				addParallel(new CommandGroup() {
-					{
-						addSequential(new Wait(time * percent));
-						addSequential(
-								new ArmElevatorSetPosition(Arm.Positions.SCALE_HIGH, Elevator.Positions.SCALE_HIGH));
-					}
-				});
-				addSequential(new FollowVelocityPath(profile));
-			}
-		});
-
-		// addSequential(new PreAuto(Arm.Positions.TUCK));
-		// addSequential(new ArmElevatorSetPosition(Arm.Positions.SCALE,
+		// addSequential(new CommandGroup() {
+		// {
+		// addParallel(new CommandGroup() {
+		// {
+		// addSequential(new Wait(time * percent));
+		// addSequential(
+		// new ArmElevatorSetPosition(Arm.Positions.SCALE_HIGH,
 		// Elevator.Positions.SCALE_HIGH));
-		addSequential(new ManipAutoEject(0.5));
+		// }
+		// });
+		// addSequential(new FollowVelocityPath(profile));
+		// }
+		// });
+		//
+		// // addSequential(new PreAuto(Arm.Positions.TUCK));
+		// // addSequential(new ArmElevatorSetPosition(Arm.Positions.SCALE,
+		// // Elevator.Positions.SCALE_HIGH));
+		// addSequential(new ManipAutoEject(0.5));
 	}
 }
