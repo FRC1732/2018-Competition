973048eb79a42ec2dd69c4a083106605b6d11687
Jay Schauer <lacrossejason@gmail.com>
2018-03-12 00:14:27 -0500

    simplified sensor checking, not sure what thread stuff was - not needed at all

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/Sensor.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/Sensor.java
@@ -2,4 +2,6 @@ package org.usfirst.frc.team1732.robot.sensors;
 
 public interface Sensor {
 	public boolean isWorking();
+
+	public String getName();
 }
--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/SensorChecker.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/SensorChecker.java
@@ -5,40 +5,19 @@ import java.util.LinkedList;
 import org.usfirst.frc.team1732.robot.util.Debugger;
 
 public class SensorChecker {
-	
-	private static boolean enabled = false;
-	private static LinkedList<SensorChecker> sensors = new LinkedList<>();
-	
+
+	private static LinkedList<Sensor> sensors = new LinkedList<>();
+
 	public static void addSensor(Sensor s, String name) {
-		if (enabled) {
-			throw new RuntimeException(
-					"Sensor Checking loop is already started, so you can't add new sensors.");
-		}
-		sensors.add(new SensorChecker(s, name));
+		sensors.add(s);
 	}
-	
-	public static void run() {
-		enabled = true;
-		while (true) {
-			for(SensorChecker s : sensors) {
-				s.check();
-				Thread.yield();
+
+	public static void checkSensors() {
+		for (Sensor s : sensors) {
+			if (!s.isWorking()) {
+				Debugger.logSimpleInfo("Sensor error: " + s.getName());
 			}
 		}
 	}
-	
-	private Sensor s;
-	private String name;
-	
-	private SensorChecker(Sensor s, String name) {
-		this.s = s;
-		this.name = name;
-	}
-	
-	private void check() {
-		if (!s.isWorking()) {
-			Debugger.logSimpleInfo("Sensor error: " + name);
-		}
-	}
-	
+
 }
