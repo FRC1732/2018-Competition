0c95d97a30f49f7cfca5e899f2710511582c5e7d
Jay Schauer <lacrossejason@gmail.com>
2018-03-12 00:28:38 -0500

    cleaned up stuff, also current implementation of atZero wasn't helpful for checking sensors

--- 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/SensorChecker.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team1732/robot/sensors/SensorChecker.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1732.robot.sensors;
 
 import java.util.LinkedList;
+import java.util.function.BooleanSupplier;
 
 import org.usfirst.frc.team1732.robot.util.Debugger;
 
@@ -8,9 +9,9 @@ public class SensorChecker {
 
 	private static LinkedList<Runnable> sensors = new LinkedList<>();
 
-	public static void addSensor(Sensor s, String name) {
+	public static void addSensor(BooleanSupplier s, String name) {
 		sensors.add(() -> {
-			if (!s.isWorking()) {
+			if (!s.getAsBoolean()) {
 				Debugger.logSimpleInfo("Sensor error: " + name);
 			}
 		});
